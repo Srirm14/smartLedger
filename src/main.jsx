84688268ca3be863fe3743b6c8a9442601f18eae
Sriram.Leo <sriram.leo@deltaww.com>
2026-03-30 14:48:32 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import ScrollableApp from "./ScrollableApp ";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./utils/queryClient";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = createRoot(document.getElementById("root"));
 
@@ -24,6 +25,7 @@ void prepare().then(() => {
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
+          <Analytics />
           <Toaster />
           <ScrollableApp>
             <App />
